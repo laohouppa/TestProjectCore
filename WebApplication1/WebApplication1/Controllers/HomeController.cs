a0f5973605be6876fd7fcabb7aaa0eb329773e97
TOTO <vietminh.nguyen@msn.com>
2019-04-02 15:30:48 +0200

    test

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace WebApplication1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
